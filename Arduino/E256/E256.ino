2fab961983f9f601e81045da4ced9d48e56b7bda
maurinElectroTextile <maurin@datapaulette.org>
2018-02-17 21:34:17 +0100

    Need to fix index_A & index_B

--- Arduino/E256/E256.ino
+++ Arduino/E256/E256.ino
@@ -16,28 +16,27 @@ void setup() {
 
   SPI.beginTransaction(SPISettings(14000000, MSBFIRST, SPI_MODE0));
   SPI.begin();              // Start the SPI module
-
   pinMode(SS, OUTPUT);      // Set up slave mode
-  digitalWrite(SS, LOW);    // set latchPin LOW
-  digitalWrite(SS, HIGH);   // set latchPin HIGH
+  digitalWrite(SS, LOW);    // Set latchPin LOW
+  digitalWrite(SS, HIGH);   // Set latchPin HIGH
 
   ///// SETUP ADC_0 ////
   adc->setAveraging(1, ADC_0);                                        // Set number of averages
   adc->setResolution(8, ADC_0);                                       // Set bits of resolution
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_0);   // Change the conversion speed
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_0);       // Change the sampling speed
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_0);   // Set the conversion speed
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_0);       // Set the sampling speed
   // always call the compare functions after changing the resolution!
-  adc->enableCompare(1.0 / 3.3 * adc->getMaxValue(ADC_0), 0, ADC_0);  // measurement will be ready if value < 1.0V
-  //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_0)/3.3, 2.0*adc->getMaxValue(ADC_0)/3.3, 0, 1, ADC_0); // ready if value lies out of [1.0,2.0] V
+  adc->enableCompare(1.0 / 3.3 * adc->getMaxValue(ADC_0), 0, ADC_0);  // Measurement will be ready if value < 1.0V
+  //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_0)/3.3, 2.0*adc->getMaxValue(ADC_0)/3.3, 0, 1, ADC_0); // Ready if value lies out of [1.0,2.0] V
 
   ////// SETUP ADC_1 /////
-  adc->setAveraging(1, ADC_1);                                        // set number of averages
-  adc->setResolution(8, ADC_1);                                       // set bits of resolution
-  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1);   // change the conversion speed
-  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1);       // change the sampling speed
+  adc->setAveraging(1, ADC_1);                                        // Set number of averages
+  adc->setResolution(8, ADC_1);                                       // Set bits of resolution
+  adc->setConversionSpeed(ADC_CONVERSION_SPEED::HIGH_SPEED, ADC_1);   // Set the conversion speed
+  adc->setSamplingSpeed(ADC_SAMPLING_SPEED::HIGH_SPEED, ADC_1);       // Set the sampling speed
   // always call the compare functions after changing the resolution!
-  adc->enableCompare(1.0 / 3.3 * adc->getMaxValue(ADC_1), 0, ADC_1);  // measurement will be ready if value < 1.0V
-  //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_1)/3.3, 2.0*adc->getMaxValue(ADC_1)/3.3, 0, 1, ADC_1); // ready if value lies out of [1.0,2.0] V
+  adc->enableCompare(1.0 / 3.3 * adc->getMaxValue(ADC_1), 0, ADC_1);  // Measurement will be ready if value < 1.0V
+  //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_1)/3.3, 2.0*adc->getMaxValue(ADC_1)/3.3, 0, 1, ADC_1); // Ready if value lies out of [1.0,2.0] V
 
   adc->startSynchronizedContinuous(PIN_A9, PIN_A3);
   serial.send(myPacket, ROW_FRAME);
@@ -47,26 +46,27 @@ void setup() {
 void loop() {
 
   uint16_t setCols = 0x8080; // 1000 0000 1000 0000
-
-  for (uint8_t row = 0; row < DUAL_ROWS; row++) {
-
+  
+  for (uint8_t col = 0; col < DUAL_COLS; col++) {
     setCols = setCols >> 1;
+    for (uint8_t row = 0; row < DUAL_ROWS; row++) {
 
-    digitalWrite(SS, LOW);              // Set latchPin LOW
-    SPI.transfer(lowByte(setCols));     // Shift out the terird byte
-    SPI.transfer(highByte(setCols));    // Shift out the secound byte
-    SPI.transfer(dualSetRows[row]);     // Shift out the first byte
-    digitalWrite(SS, HIGH);             // Set latchPin HIGH
+      digitalWrite(SS, LOW);              // Set latchPin LOW
+      SPI.transfer(lowByte(setCols));     // Shift out the terird byte
+      SPI.transfer(highByte(setCols));    // Shift out the secound byte
+      SPI.transfer(dualSetRows[row]);     // Shift out the first byte
+      digitalWrite(SS, HIGH);             // Set latchPin HIGH
 
-    //result = adc->analogSynchronizedRead(PIN_A9, PIN_A3);
-    result = adc->readSynchronizedContinuous();
+      //result = adc->analogSynchronizedRead(PIN_A9, PIN_A3);
+      result = adc->readSynchronizedContinuous();
 
-    uint8_t index_A = col * COLS + row;                // Compute the unidimensional array index for sensorA
-    uint8_t index_B = col * COLS + row + ROW_FRAME_2;  // Compute the unidimensional array index for sensorB
+      uint8_t index_A = col * COLS + row;         // Compute the unidimensional array index_A (FIXME)
+      uint8_t index_B = col * COLS + row + 8;     // Compute the unidimensional array index_B (FIXME)
 
-    myPacket[index_A] = result.result_adc0;
-    myPacket[index_B] = result.result_adc1;
+      myPacket[index_A] = result.result_adc0;     // Write ADC0 value to the unidimensional array
+      myPacket[index_B] = result.result_adc1;     // Write ADC1 value to the unidimensional array
 
+    }
   }
   serial.update();
 }
