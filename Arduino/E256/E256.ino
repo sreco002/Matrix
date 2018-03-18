f3ba082d2942b0953c4007986e65d2e82c40684f
maurinElectroTextile <maurin@datapaulette.org>
2018-03-18 23:43:57 +0100

    Added Serial debug

--- Arduino/E256/E256.ino
+++ Arduino/E256/E256.ino
@@ -1,16 +1,15 @@
 // eTextile matrix sensor - E-256 (shield V2.0)
 #include "E256.h"
 
-
 ////////////////////////////////////// SETUP
 void setup() {
 
   pinMode(A0_PIN, INPUT);
   pinMode(A1_PIN, INPUT);
 
-  // Serial.begin(BAUD_RATE);
-  serial.setPacketHandler(&onPacket); // We must specify a packet handler method so that
-  serial.begin(BAUD_RATE);  // Start the serial module
+  Serial.begin(BAUD_RATE);
+  // serial.setPacketHandler(&onPacket); // We must specify a packet handler method so that
+  // serial.begin(BAUD_RATE);  // Start the serial module
 
   pinMode(SS, OUTPUT);      // Set up slave mode
   digitalWrite(SS, LOW);    // Set latchPin LOW
@@ -37,18 +36,19 @@ void setup() {
   //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_1)/3.3, 2.0*adc->getMaxValue(ADC_1)/3.3, 0, 1, ADC_1); // Ready if value lies out of [1.0,2.0] V
 
   adc->startSynchronizedContinuous(A0_PIN, A1_PIN);
+
   // serial.send(myPacket, ROW_FRAME);
 }
 
 ////////////////////////////////////// LOOP
 void loop() {
-  /*
-    if ((millis() - lastFarme) >= 1000) {
-      Serial.printf(F("\nFPS: %d"), fps);
-      lastFarme = millis();
-      fps = 0;
-    }
-  */
+
+  if ((millis() - lastFarme) >= 1000) {
+    Serial.printf(F("\nFPS: %d"), fps);
+    lastFarme = millis();
+    fps = 0;
+  }
+
   // Columns are digital OUTPUT PINS
   // Rows are analog INPUT PINS
   // uint16_t setCols = 0x8080; // Powering two cols at a time (NOTGOOD) -> 1000 0000 1000 0000
@@ -73,8 +73,17 @@ void loop() {
     }
     setCols = setCols >> 1;
   }
-  serial.update();
-  // fps++;
+
+  for (int i = 0; i < index ; i++) {
+    Serial.printf(F(" %d"), myPacket[index]);
+  }
+  Serial.printf(F("\n"));
+
+  index = 0;
+
+  // serial.update();
+
+  fps++;
 }
 
 // This is our packet callback, the buffer is delivered already decoded.
